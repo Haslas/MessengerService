884d8ecf69565eff4ab8a8d74545fc46cf7964aa
Haslas <haslas56@gmail.com>
2018-03-07 22:07:55 +0000

    Update outbox - 1.7.py

--- outbox - 1.7.py
+++ outbox - 1.7.py
@@ -1,51 +1,55 @@
 print("====OUTBOX====")
-print("Version: 1.6 - The Login")
+print("Version: 1.6")
 import socket
-ip="10.110.211.179"
-inboxPort = 666
 
-#===================================================================#
-"""
-def setupServerConn(ip,port):
-    global serversocket
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind((ip,port))
-    serversocket.listen(5)
-"""
-    
-def onConnect():
-            print("Connecting to server...")
-            clientsocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            clientsocket.connect((ip,inboxPort))
-            print("Connection established")
-            username = input("Enter username: ")
-            clientsocket.send(bytes(username,"utf-8"))
-            print("Username sent to server.")
-            print("Second server connection established")
-            print("Waiting for reply from server")
-            ack = clientsocket.recv(1024)
-            print("Server reply recieved")
-            ack = ack.decode("utf-8")
-            if ack != "0":
-                    print("Username already taken.   Please choose another.")
-                    clientsocket.close()
-                    onConnect()
-            else:
-                    print("Username Accepted")
-                    clientsocket.close()
+def register():
+    print("Connecting to server...")
+    clientsocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect((ip,outboxPort))
+    print("Connection established")
+    username = input("Enter username: ")
+    clientsocket.send(bytes(username,"utf-8"))
+    print("Username sent to server.")
+    print("Waiting for reply from server")
+    ack = clientsocket.recv(1024)
+    print("Server reply recieved")
+    ack = ack.decode("utf-8")
+    if ack != "0":
+            print("Username already taken.   Please choose another.")
+            clientsocket.close()
+            usernameAccepted=0
+    else:
+            print("Username Accepted")
+            clientsocket.close()
+            usernameAccepted=1
+    return usernameAccepted
 
-#===================================================================#
 
-#RUN FROM YOUR PROBLEMS
+def registerLoop():
+    usernameAccepted=0
+    while not usernameAccepted:
+        usernameAccepted=register()
 
-onConnect()
+def sendMessage():
+    message=""
+    while len(message) <1:
+        message = input("Enter message: ")
+    clientsocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect((ip,outboxPort))
+    clientsocket.send(bytes(message,"UTF-8"))
+    clientsocket.close()
 
-message=""
+
+
+
+ip=input("IP Address: ")
+outboxPort = ""
+while type(outboxPort)!=int:
+    outboxPort=input("Outbox Port: ")
+    try:
+        outboxPort=int(outboxPort)
+    except ValueError:
+        print("This should be a number.")
+registerLoop()
 while 1:
-        while len(message) <1:
-            message = input("Enter message: ")
-        clientsocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clientsocket.connect((ip,inboxPort))
-        clientsocket.send(bytes(message,"UTF-8"))
-        clientsocket.close()
-        message=""
+        sendMessage()
