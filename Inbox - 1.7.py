3fa856dbe5be3071d75a2d0ecbc3433f96fdfc86
Haslas <haslas56@gmail.com>
2018-03-07 09:20:53 +0000

    Update Inbox - 1.7.py

--- Inbox - 1.7.py
+++ Inbox - 1.7.py
@@ -3,13 +3,20 @@ print("Version: 1.5 - The Login")
 import socket
 inboxPort = 5996
 
-
-
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(("",inboxPort))
-serversocket.listen(5)
-while True:
+def acceptConnection(serversocket):
         connection, address = serversocket.accept()
         buf = connection.recv(1024)
         buf = buf.decode("utf-8")
         print(buf)
+
+def declareServerSocket(inboxPort):
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversocket.bind(("",inboxPort))
+        serversocket.listen(5)
+        return serversocket
+        
+serversocket=declareServerSocket(inboxPort)      
+while True:
+        acceptConnection(serversocket)
+
+   
